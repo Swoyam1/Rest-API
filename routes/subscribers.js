5b5f9682595227c43b47cd3a1255b17903499e91
swoyam1 <swoyam1.pant@gmail.com>
2023-07-06 03:19:55 +0530

    routes updated

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -11,8 +11,8 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-    res.send(req.params.id);
+router.get('/:id', getSubscriber, (req, res) => {
+    res.json(res.subscriber);
 });
 
 router.post('/', async (req, res) => {
@@ -28,12 +28,43 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.patch('/:id', (req, res) => {
-
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if(req.body.name != null) {
+        res.subscriber.name = req.body.name;
+    }
+    if(req.body.subscribedToChannel != null) {
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+    }
+    try {
+        const updateSubscriber = await res.subscriber.save();
+        res.json(updateSubscriber);
+    } catch(err) {
+        res.status(400).json({message: err.message});
+    }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+        await res.subscriber.remove();
+        res.json({message: "Deleted Subscriber."});
 
+    } catch(err) {
+        res.status(500).json({message: err.message});
+    }
 })
+
+async function getSubscriber(req, res, next) {
+    let subscriber;
+    try{
+        subscriber = await Subscriber.findById(req.params.id);
+        if(subscriber == null) {
+            return res.status(404).json({message: "Cannot find subscriber"});
+        }
+    } catch(err) {
+        return res.status(500).json({message: err.message});
+    }
+    res.subscriber = subscriber;
+    next();
+}
 
 module.exports = router;
